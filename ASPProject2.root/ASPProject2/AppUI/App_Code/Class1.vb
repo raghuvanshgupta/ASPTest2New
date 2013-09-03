b1f89dbaf92bcaf2e165772fceef320809281ea7
Vss2 <vss2@localhost>
2013-09-03 09:44:31 +0000

    Vss2Git

--- ASPProject2.root/ASPProject2/AppUI/App_Code/Class1.vb
+++ ASPProject2.root/ASPProject2/AppUI/App_Code/Class1.vb
@@ -1,5 +1,5 @@
 ﻿Imports Microsoft.VisualBasic
 
 Public Class Class1
-    
+    'Added by VSS2    
 End Class
--- ASPProject2.root/ASPProject2/ClassLibrary/BusinessLayer/Class1.vb
+++ ASPProject2.root/ASPProject2/ClassLibrary/BusinessLayer/Class1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Class1
+    'Added by VSS2    
+    'Added by Raghu    
+    'Added by Govind   
+    'Added by Abhishek  
+
 
 End Class
